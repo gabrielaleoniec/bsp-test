07ca082ad6999cbd3f76cba2c9731d8a5d66b182
Gabriela Leoniec <gabriela.leoniec@cloudnine.se>
2026-02-04 18:46:17 +0100

    feat: basic layout prepared

---
+++ src/components/page-layout/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="flex items-center justify-center border-b-stone-600 border-b py-2 px-8 bg-stone-50 ">
+      <h1>Simplistic PIM for testing</h1>
+    </div>
+  );
+};
+
+export default Header;
